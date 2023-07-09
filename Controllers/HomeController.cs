ff8d44aba7f20b8d30d4e9be2d28f6eda5410591
Sanskar Shrestha <elaiextha@gmail.com>
2023-07-09 18:04:17 +0545

    Single user location track working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Firebase.Auth;
+using Google.Cloud.Firestore;
 using Hikeyy.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -9,16 +10,37 @@ namespace Hikeyy.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        
-
+        private readonly FirestoreService _firestoreService;
+        private FirestoreDb _db;
         FirebaseAuthProvider auth;
         public HomeController(ILogger<HomeController> logger)
         {
             auth = new FirebaseAuthProvider(
                             new FirebaseConfig("AIzaSyC_qh0TxNX0RXDuPfB5tfOO86GnAx3dY6Q"));
-           
+            System.Diagnostics.Debug.WriteLine("COORDINATES KO CONSTRUCTOR CALLED HAI");
+            var credentialsPath = "hikeyyy-firebase-adminsdk-oj70g-ca14e13827.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialsPath);
+
+            // Initialize Firestore client
+            _db = FirestoreDb.Create("hikeyyy");
+
         }
 
+        [HttpGet]
+        public async Task<JsonResult> GetLocations()
+        {
+            CollectionReference locationsRef = _db.Collection("testusers");
+            QuerySnapshot snapshot = await locationsRef.GetSnapshotAsync();
+
+            List<Dictionary<string, object>> locations = new List<Dictionary<string, object>>();
+            foreach (DocumentSnapshot document in snapshot.Documents)
+            {
+                Dictionary<string, object> locationData = document.ToDictionary();
+                locations.Add(locationData);
+            }
+
+            return Json(locations);
+        }
         public IActionResult Index()
         {
             var token = HttpContext.Session.GetString("_UserToken");
