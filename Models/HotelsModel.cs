50706f714cc48f7448615e423a8805316589fcaa
ssthaisha <ssthaisha01@gmail.com>
2023-07-13 22:53:58 +0545

    hotels added

---
+++ Models/HotelsModel.cs
@@ -0,0 +1,17 @@
+﻿using Google.Cloud.Firestore;
+
+namespace Hikeyy.Models
+{
+    [FirestoreData]
+    public class HotelsModel
+    {
+        [FirestoreProperty]
+        public string? Name { get; set; }
+
+        [FirestoreProperty]
+        public string? Phone { get; set; }
+
+        [FirestoreProperty]
+        public string? Location { get; set; }
+    }
+}
